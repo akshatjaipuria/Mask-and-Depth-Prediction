214c366c5924c4cd20426eada2f38b3f5ba37a09
Akshat Jaipuria <akshat.jaipuria504@gmail.com>
2020-05-19 20:46:46 +0530

    added features

--- train.py
+++ train.py
@@ -24,12 +24,13 @@ class train_model:
             loss.backward()  # calculating gradients
             optimizer.step()  # updating weights
             if self.tb:
+                self.globaliter += 1
                 self.tb.save_value('Train Loss', 'train_loss', self.globaliter, loss.item())
 
             if batch_idx % 50 == 0:
                 metric_value = 0
                 if metric:
-                    metric_value = metric(output, data['o1']).cpu().detach().numpy()
+                    metric_value = metric(output, data['o1']).cpu().detach().numpy() / output.shape[0]
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tMetric: {:.6f}'.format(
                     epoch, batch_idx * len(data['i1']), len(train_loader.dataset), 100. * batch_idx / len(train_loader),
                     loss.item(), metric_value))
@@ -39,18 +40,20 @@ class train_model:
                 # len(dataloader) --> total no of batches, each to specified size like 16
 
             if batch_idx % 500 == 0:
-                show_image(output, n_row=8, title='Predicted (Training)')
+                show_image(data['o1'][::4].cpu(), n_row=8, title='Target (Training)')
+                show_image(output[::4].cpu(), n_row=8, title='Predicted (Training)')
                 # print(output)
                 # print(data['o1'])
 
             if batch_idx % 1000 == 0:
                 torch.save(model.state_dict(), self.path / f"{batch_idx}.pth")
 
-    def validate(self, model, criterion, valid_loader):
+    def validate(self, model, criterion, metric, valid_loader):
         # setting model evaluate mode, takes care of batch norm, dropout etc. not required while validation
         model.eval()
         valid_loss = 0
         correct = 0
+        metric_value = 0
         with torch.no_grad():
             for batch_idx, data in enumerate(valid_loader):
                 data['i1'] = data['i1'].to(self.device, dtype=torch.float)
@@ -59,10 +62,14 @@ class train_model:
                 output = model(data['i1'], data['i2'])
                 loss = criterion(output, data['o1'])
                 valid_loss += loss.item()  # loss.view(loss.shape[0], -1).sum(1).mean().item()
+                if metric:
+                    metric_value += metric(output, data['o1']).cpu().detach().numpy() / output.shape[0]
+        metric_value /= len(valid_loader)
         valid_loss /= len(valid_loader)
-        print("Some predicted samples:")
-        show_image(output.cpu(), n_row=8, title='Predicted (validation)')
-        print("Average Validation loss: ", valid_loss)
+        print("Some target vs predicted samples:")
+        show_image(data['o1'][::4].cpu(), n_row=8, title='Target (validation)')
+        show_image(output[::4].cpu(), n_row=8, title='Predicted (validation)')
+        print("Average Validation loss: {}\t Average IOU: {}".format(valid_loss, metric_value))
 
     def run_model(self, model, train_loader, valid_loader, criterion, metric=None, lr=0.01, epochs=10):
 
@@ -70,4 +77,4 @@ class train_model:
 
         for epoch in range(1, epochs + 1):
             self.train(model, criterion, metric, train_loader, optim, epoch)
-            self.validate(model, criterion,  valid_loader)
+            self.validate(model, criterion,  metric, valid_loader)
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import torchvision
 import matplotlib.pyplot as plt
 from torchsummary import summary
 import torch
+from torch import nn
 
 
 def show_image(inp, n_row=8, title=None):
@@ -26,3 +27,26 @@ def model_summary(net, size):
     model = net.to(device)
     print(summary(model, input_size=size))
     return device
+
+
+def dice_coeff(pred, target):
+    smooth = 1.
+    num = pred.size(0)
+    m1 = pred.view(num, -1)  # Flatten
+    m2 = target.view(num, -1)  # Flatten
+    intersection = (m1 * m2).sum()
+
+    return (2. * intersection + smooth) / (m1.sum() + m2.sum() + smooth)
+
+
+class SoftDiceLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(SoftDiceLoss, self).__init__()
+
+    def forward(self, logits, targets):
+        probs = F.sigmoid(logits)
+        num = targets.size(0)  # Number of batches
+
+        score = dice_coeff(probs, targets)
+        score = 1 - score.sum() / num
+        return score
