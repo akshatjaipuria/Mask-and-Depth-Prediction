93b8b0e882b743d58653f54302ca9eebd394d3af
Akshat Jaipuria <akshat.jaipuria504@gmail.com>
2020-05-19 09:35:05 +0530

    Update network_architecture_1.py

--- model/network_architecture_1.py
+++ model/network_architecture_1.py
@@ -7,13 +7,26 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.drop = drop
 
-        self.convblock1 = nn.Sequential(
+        self.convblock1_1 = nn.Sequential(
             nn.Conv2d(3, 32, 3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(32),
             nn.ReLU()
         )
 
-        self.convblock2 = nn.Sequential(
+        self.convblock1_2 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, stride=1, padding=1, bias=False, groups=32),
+            nn.Conv2d(32, 64, 1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+        )
+
+        self.convblock2_1 = nn.Sequential(
+            nn.Conv2d(3, 32, 3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU()
+        )
+
+        self.convblock2_2 = nn.Sequential(
             nn.Conv2d(32, 32, 3, stride=1, padding=1, bias=False, groups=32),
             nn.Conv2d(32, 64, 1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(64),
@@ -21,41 +34,39 @@ class Net(nn.Module):
         )
 
         self.convblock3 = nn.Sequential(
-            nn.Conv2d(64, 64, 3, stride=1, padding=1, bias=False),
+            nn.Conv2d(128, 64, 1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU()
         )
 
         self.convblock4 = nn.Sequential(
-            nn.Conv2d(64, 64, 3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU()
-        )
-
-        self.convblock5 = nn.Sequential(
             nn.Conv2d(64, 128, 3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(128),
             nn.ReLU()
         )
 
+        self.convblock5 = nn.Sequential(
+            nn.Conv2d(128, 256, 3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU()
+        )
+
         self.convblock6 = nn.Sequential(
             nn.Conv2d(128, 1, 3, stride=1, padding=1, bias=False),
-            nn.sigmoid()
         )
 
     def forward(self, input1, input2):
         x1 = input1
         x2 = input2
 
-        x1 = self.convblock2(self.convblock1(x1))
-        x2 = self.convblock2(self.convblock1(x2))
+        x1 = self.convblock1_2(self.convblock1_1(x1))
+        x2 = self.convblock2_2(self.convblock2_1(x2))
 
-        x_1 = (x1 + x2)
+        x_1 = torch.cat([x1, x2], dim=1)
         x = self.convblock3(x_1)
         x = self.convblock4(x)
-        x = nn.ReLU()(x + x_1)
-        x = torch.cat([x1, x2], dim=1)
-
-        x = self.convblock6(self.convblock5(x))
+        x = (x + x_1)
+        x = self.convblock5(x)
+        x = self.convblock6(x)
 
         return x
